4b522ddcc92368078a5da10169238350f29e2d56
BRobinson404 <brobinso123@gmail.com>
2023-07-15 14:35:01 -0500

    created NumberOfEvents
    created component to handle rendering the correct number of events from the list

---
+++ src/components/NumberOfEvents.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NumberOfEvents = ({ eventNumber, onEventNumberChange }) => {
+  const handleInputChanged = (value) => {
+    onEventNumberChange(value);
+  };
+  console.log(eventNumber);
+
+  return (
+    <div id="number-of-events">
+      <input
+        type="text"
+        className="textbox"
+        placeholder="Enter a number"
+        value={eventNumber}
+        onChange={(e) => handleInputChanged(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default NumberOfEvents;
